5127e75403a4cf1568c670114e15de00b6c4c2c7
Suraj Choubey <srjchoubey2@gmail.com>
2021-02-07 22:43:38 +0530

    Lost track of commit commiting since start

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css'
+import './index.css';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap-social/bootstrap-social.css';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
